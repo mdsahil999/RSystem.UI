2128da5dd12a54e72f9cf2aca4ca28ae082af878
sahil <md.sahil620@gmail.com>
2025-04-15 23:36:27 +0530

    code clean

--- src/app/loader-module/loader/loader.component.spec.ts
+++ src/app/loader-module/loader/loader.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { LoaderComponent } from './loader.component';
 import { LoaderService } from '../services/loader.service';
 
